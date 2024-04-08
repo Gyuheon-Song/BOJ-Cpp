972ba98d39edb97793ef2c900d544c3a07ff4495
gyuheonSong <gyuheonsong@naver.com>
2024-04-08 14:52:54 +0900

    #26517 S5 Implementation

---
+++ Implementation/26517.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main() {
+    long long k, a, b, c, d;
+    cin >> k;
+    cin >> a >> b >> c >> d;
+    if (a * k + b == c * k + d) {
+        cout << "Yes" << ' ' << a * k + b;
+    }
+    else {
+        cout << "No";
+    }
+
+    return 0;
+}
